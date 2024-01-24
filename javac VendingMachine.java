bc7db6deaf133f7c5c437b5f8c14d0e010a9923a
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-01-24 15:25:15 +0800

    Update javac VendingMachine.java

--- javac VendingMachine.java
+++ javac VendingMachine.java
@@ -7,7 +7,7 @@ public class VendingMachine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+  
         while (true) {
             displayMenu();
 
